d860c9a62d851c0fe1c7700740699b086b8c933a
Kate Wood <katewood611@gmail.com>
2017-01-19 21:21:29 -0800

    adds atom packages

---
+++ scripts/settings/install_atom_packages.sh
@@ -0,0 +1,7 @@
+#-------------------------------------------------------------------------------
+# Install Atom Packages (settings/install_atom_packages.sh)
+#-------------------------------------------------------------------------------
+
+inform "Installing atom packages in the ~/.atom/packages directory..." true
+apm install atom-beautify autosave-plus color-picker emmet erb-helper file-icons highlight-selected minimap minimap-highlight-selected open-in-browsers open-recent pigments react seti-ui white-cursor sublime-style-column-selection
+show "Complete!"
